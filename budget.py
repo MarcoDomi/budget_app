f814465d9543d392d7663541c7ade951c92b1b67
Marco Dominguez <marcod082@gmail.com>
2023-09-08 17:02:10 -0500

    added some files

--- budget.py
+++ budget.py
@@ -1,25 +1,40 @@
 
-class budget_app:
-    income = 0.0
-    expense_name = []
-    expenses_amount = []
+class budget:
+    monthly_income = 0.0
+    expense_count = 0
+    expense_names = []
+    expense_amounts = []
     total_expense_amount = 0.0
     
     def __init__(self, first_name, last_name) -> None:
         self.first_name = first_name
         self.last_name = last_name
         
-    def set_income(self,value) -> None:
+    def set_monthly_income(self,value) -> None:
         try:
-            self.income = float('%.2f'%value)
+            self.monthly_income = float('%.2f'%value)
         except ValueError:
             print("Non-numerical values are not valid.")
 
     def add_expense(self, name, amount):
-        pass
+        self.expense_names =+ [name]
+        self.expense_amounts =+ [amount]
+        self.total_expense_amount =+ amount
+        self.expense_count =+ 1
+
+    def print_expenses(self):
+        print(self.first_name, self.last_name)
+        total_len = len(self.first_name) + len(self.last_name)
+        print("-"*(total_len + 1)) #add 1 to length so the dash bar length matches the length of first and last name
+
+        for i in range(self.expense_count):
+            print(f"{self.expense_names[i]}:${self.expense_amounts[i]}")
+
+        print("Total expenses: ",self.total_expense_amount)
 
 
-        
-b = budget_app("max","domi")
-b.set_income(4.33)
-print(b.income)
+
+#b = budget("max","domi")
+#b.set_monthly_income(4.33)
+#b.print_expenses()
+
---
+++ main.py
@@ -0,0 +1,6 @@
+from budget import budget
+
+
+
+my_budget = budget("Marco", "Dominguez")
+my_budget.print_expenses()
