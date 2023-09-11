7f4bb5912131f6df9a8af760cbbb1a58144d49c1
Marco Dominguez <marcod082@gmail.com>
2023-09-11 14:22:28 -0500

    extra

--- budget.py
+++ budget.py
@@ -9,6 +9,7 @@ class budget:
     def __init__(self, first_name, last_name) -> None:
         self.first_name = first_name
         self.last_name = last_name
+      
         
     def set_monthly_income(self,value:float) -> None:
         if self.__is_valid_amount(value):
